e3bc8726205db629b7e184663c381836d07ca173
Dean Hunter <jeeka7@gmail.com>
2022-10-15 01:40:56 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,7 +1,9 @@
 import streamlit as st
-if 'pizza' not in st.session_state:
-  st.session_state.pizza = 0
-if 'purchase_quantity' not in st.session_state:
+if "count" not in st.session_state:
+	st.session_state.count = 0
+if increment:
+    st.session_state.count += 1
+if "purchase_quantity" not in st.session_state:
   st.session_state.purchase_quantity = 0
 def purchase ():
    nonlocal st.session_state.pizza
