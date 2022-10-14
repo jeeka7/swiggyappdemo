8311062d5515ae6854364d126620720bf9ae0c03
Dean Hunter <jeeka7@gmail.com>
2022-10-15 01:22:45 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -4,8 +4,8 @@ if 'pizza' not in st.session_state:
 if 'purchase__quantity' not in st.session_state:
     st.session_state.purchase__quantity = 0
 def purchase ():
-   global pizza
-   global purchase__quantity
+   global st.session_state.pizza
+   global st.session_state.purchase__quantity
    purchase__quantity += 1
    pizza += 100
 if st.button("Buy 1 more pizza"):
