18aff5d3a543d705e8770e7bec81513ca921db37
Dean Hunter <jeeka7@gmail.com>
2022-10-15 01:02:11 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,8 +1,9 @@
 import streamlit as st
 purchase__quantity = 0
 if purchase__quantity == 0:
-    pizza = 500
+pizza = 500
 def purchase ():
+   global pizza
    global purchase__quantity
    purchase__quantity += 1
    pizza += 100
