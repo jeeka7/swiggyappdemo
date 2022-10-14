8e1e39d7197c316a7a7c33e5afb3f0c7ed57a6a4
Dean Hunter <jeeka7@gmail.com>
2022-10-15 01:09:22 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,8 +1,9 @@
 import streamlit as st
 purchase__quantity = 0
 if 'pizza' not in st.session_state:
-    st.session_state['pizza'] = 500
-
+    st.session_state.pizza = 500
+if 'purchase__quantity' not in st.session_state:
+    st.session_state.purchase__quantity = 0
 def purchase ():
    global pizza
    global purchase__quantity
