35cef481fee89174fd0768fb24af7e2c1e68dc51
Dean Hunter <jeeka7@gmail.com>
2022-10-15 00:52:58 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -3,6 +3,7 @@ purchase__quantity = 0
 if purchase__quantity == 0:
     pizza = 500
 def purchase ():
+   global purchase__quantity
    purchase__quantity += 1
    pizza += 100
 if st.button("Buy 1 more pizza"):
