7e1a24a7db0f3bef5befc00b3fc2322df506ec0f
Dean Hunter <jeeka7@gmail.com>
2022-10-15 01:26:50 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,12 +1,12 @@
 import streamlit as st
 if 'pizza' not in st.session_state:
   st.session_state.pizza = 0
-if 'purchase__quantity' not in st.session_state:
-    st.session_state.purchase__quantity = 0
+if 'purchase_quantity' not in st.session_state:
+  st.session_state.purchase_quantity = 0
 def purchase ():
    global st.session_state.pizza
-   global st.session_state.purchase__quantity
-   purchase__quantity += 1
+   global st.session_state.purchase_quantity
+   purchase_quantity += 1
    pizza += 100
 if st.button("Buy 1 more pizza"):
     purchase()
